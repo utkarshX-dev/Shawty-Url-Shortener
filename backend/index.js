f26039ffe37689eb7efdc12ac20822b735c5aaf6
Utkarsh <bhandariutkarsh01@gmail.com>
2026-05-25 11:54:36 +0530

    fixed cors issue

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://ushawty.vercel.app/"],
+    origin: ["http://localhost:5173", "https://ushawty.vercel.app"],
     credentials: true
 }));
 
